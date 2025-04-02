f811fd85a2eb2669ab0704b288fd5f4ca7544bd0
GRUPPY02 <GRUPPY02@DESKTOP-B3K8HS6>
2025-04-02 09:56:06 -0300

    Melhorando implementacao de imprimirTabuleiro

--- Xadrez_console/Program.cs
+++ Xadrez_console/Program.cs
@@ -7,7 +7,7 @@ Tabuleiro tab = new Tabuleiro(8, 8);
 
 tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
 tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(2, 9));
+tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(2, 3));
 tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
 
 
